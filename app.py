0f432202f85f1947ed473ffd61a3731340915627
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-01-22 10:59:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Flask webapp demo 1
+"""
+from flask import Flask
+from flask import request
+from flask import make_response
+
+app= Flask(__name__)
+@app.route("/webhook",method=['POST'])
+def webhook():
+    req=request.get_json(silent=True,force=True)
+    print("Request:")
+    print(json.dumps(req,indent=4))
+    speech=process()
+    res=make_response(speech)
+    return res
+def process():
+    return {
+    "speech": "Welcome to nWave Chatbot",
+    "displayText": "Welcome to nWave Chatbot",
+    "source": "nWave_webhook"
+    }
+if(__name__=='__main__'):
+    app.run(use_reloader=True,debug=True)
