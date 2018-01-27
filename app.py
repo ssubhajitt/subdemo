999ff528419c3f2832827ef48b77cfb15f02722d
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-01-27 17:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,13 @@ from flask import request
 from flask import make_response
 
 app= Flask(__name__)
-@app.route("/webhook",methods=['POST'])
+@app.route("/webhook")
 def webhook():
-    req=request.get_json(silent=True,force=True)
-    print("Request:")
-    print(json.dumps(req,indent=4))
-    speech=process()
-    res=make_response(speech)
-    return res
-def process():
-    return {
+    return """{
     "speech": "Welcome to nWave Chatbot",
     "displayText": "Welcome to nWave Chatbot",
     "source": "nWave_webhook"
-    }
+    }"""
+    
 if(__name__=='__main__'):
     app.run(use_reloader=True,debug=True)
