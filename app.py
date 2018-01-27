87f05ede794e849c83307610e3a9766287901f46
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-01-27 19:03:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from flask import make_response
 app= Flask(__name__)
 @app.route("/webhook",methods=['POST'])
 def webhook():
-    req=request.get_json(silent=True,force=True)
-    print("Request:")
-    print(json.dumps(req,indent=4))
     speech=process()
     res=make_response(speech)
     return res
