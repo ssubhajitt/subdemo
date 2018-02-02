10d7ddf02ed5044cb747f45cebb0bf70a2e806b9
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-02 17:46:09 +0530

    bluemix update

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 """
 Flask webapp demo 1
 """
-from flask import Flask, request, make_response
+import os
+from flask import Flask
 
-app= Flask(__name__)
-@app.route("/webhook",methods=['GET'])
-def webhook():
-    return "Welcome TO nWave Chatbot flask webservice"
+app = Flask(__name__)
 
-if(__name__=='__main__'):
-    app.run(use_reloader=True,debug=True)
+@app.route('/')
+def Welcome():
+	return 'Welcome to my app running on Bluemix!'
+
+@app.route('/myapp')
+def WelcomeToMyapp():
+    return 'Welcome again to my app running on Bluemix!'
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
