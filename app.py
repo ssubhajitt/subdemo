e154d9e2017f7cd57a78534136ce41c4c98b02b8
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-02 17:23:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ Flask webapp demo 1
 from flask import Flask, request, make_response
 
 app= Flask(__name__)
-@app.route("/webhook",methods=['POST'])
+@app.route("/webhook",methods=['GET'])
 def webhook():
-    name = request.get("username")
-    return "Welcome" + name
+    return "Welcome TO nWave Chatbot flask webservice"
 
 if(__name__=='__main__'):
     app.run(use_reloader=True,debug=True)
