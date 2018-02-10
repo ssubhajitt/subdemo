6abd7634ea59aeec9cce1b05dbfe73d3827faa41
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-10 12:22:48 +0530

    chatbot update

--- app.py
+++ app.py
@@ -2,17 +2,15 @@
 Flask webapp demo 1
 """
 import os
-from flask import Flask
+from flask import Flask , request
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
 
-@app.route('/')
-def Welcome():
-	return 'Welcome to my app running on Bluemix!'
-
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
+@app.route('/webhook',methods=['POST'])
+def webhook():
+	if request.method=='POST':
+		return "nWave Chatbot is under construction"
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
