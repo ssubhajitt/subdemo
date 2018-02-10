9d38aebbc205029f0bc98e6917de8d32057e566e
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-10 13:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,14 @@ app = Flask(__name__)
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
-	if request.method=='POST':
-		response="nwave chatbot is under construction"
-		res= {	"speech": response,"displayText": response,
-        		"source": "nWave-estimation-chatbot"
-			}
-		res = json.dumps(res, indent=4)
-   		# print(res)
-  		r = make_response(res)
-   		r.headers['Content-Type'] = 'application/json'
-    		return r
+    if request.method=='POST':
+	response="nwave chatbot is under construction"
+	res= {	"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
+	res = json.dumps(res, indent=4)
+	# print(res)r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port))
