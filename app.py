624e4817b6819b14561199b79c0944e37fad42e3
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-14 13:56:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ Flask webapp demo 1
 import os
 from flask import Flask , request, make_response
 from flask_basicauth import BasicAuth
-
 app = Flask(__name__)
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
     if request.method=='POST':
-	response="nwave chatbot is under construction"
-	res= {	"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
-	res = json.dumps(res, indent=4)
-	print(res)r = make_response(res)
-	r.headers['Content-Type'] = 'application/json'
-	return r
+        response="nwave chatbot is under construction"
+        res= {"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
+        res = json.dumps(res, indent=4)
+        print(res)r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        return r
 
 port = os.getenv('VCAP_APP_PORT', '5000')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+app.run(host='0.0.0.0', port=int(port))
