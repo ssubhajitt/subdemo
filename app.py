662e0a074305f363e6f733ece973cc986d0cb7af
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-28 16:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,8 @@ import os
 import pandas as pd
 from flask import Flask , request, make_response
 from sklearn.preprocessing import Imputer
-from sklearn.decomposition import PCA
-from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from matplotlib import pyplot as plt
-from sklearn.metrics import precision_score, \
-    recall_score, confusion_matrix, classification_report, \
-    accuracy_score, f1_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
@@ -28,31 +20,35 @@ def webhook():
         req=request.get_json(silent=True,force=True)
         result=req.get("result")
         parameters=result.get("parameters")
-        response=json.dumps(parameters, indent=4)
-        print(response)
-        ##Machine  Learning model
-        #response="nwave chatbot is under construction"
+        data=json.dumps(parameters, indent=4)
+        print(data)
+        mvRegression(data)
+        response="nwave chatbot is under construction"
         res= {"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
         res = json.dumps(res, indent=4)
         print(res)
         r = make_response(res)
         r.headers['Content-Type'] = 'application/json'
         return r
-
-def mvRegression(data):
-    dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
+    
+@app.route('/model',methods=['POST'])
+def mvRegression():
+    #dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
+    dataset=pd.read_excel("E:\Bizzy\POCs\ML\dataset.xlsx",skip_header=1)
     Y=dataset.iloc[:, 14:]
     X=dataset.iloc[:,1:14]
-    header=list(df)
+    header=list(dataset)
     imputer = Imputer()
     dataset = imputer.fit_transform(X)
     X_tr,X_te,Y_tr,Y_te=train_test_split(X,Y,test_size=0.2)
     print (X_tr.shape,Y_tr.shape,X_te.shape,Y_te.shape)
     lr=LinearRegression()
     model_lr=lr.fit(X_tr,Y_tr)
-    op_lrt=lr.predict(X_te)
     val=[1,8,1,1,2,2,2,1,1,3,3,1,1]
-    return "None"
+    ds=pd.DataFrame(val).T
+    op_lrt=lr.predict(ds)
+    weightage=op_lrt[0][0]
+    return weightage
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 
