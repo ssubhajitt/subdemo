7d503f1c8e8be4aaa9e4e38937a16c838aff1c17
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-03-05 17:05:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,7 @@ app = Flask(__name__)
 @app.route('/webhook',methods=['POST'])
 def webhook():
     req=request.get_json(silent=True,force=True)
-    result=req.get("result")
-    parameters=result.get("parameters")
-    data=json.dumps(parameters, indent=4)
-    print(data)
-    mvRegression()
+    mvRegression(req)
     response="nwave chatbot is under construction"
     res= {"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
     res = json.dumps(res, indent=4)
@@ -25,20 +21,28 @@ def webhook():
     return r
 
 
-def mvRegression():
-    dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
-    #dataset=pd.read_excel("D:/Guna/POCs/ML/nWave_effort/dataset.xlsx",skip_header=1)
+def mvRegression(req):
+    #Machine Learning Model
+    #dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
+    dataset=pd.read_excel("D:/Guna/POCs/ML/nWave_effort/dataset.xlsx",skip_header=1)
     Y=dataset.iloc[:, 14:]
     X=dataset.iloc[:,1:14]
-    header=list(dataset)
+    header=list(X)
     imputer = Imputer()
     dataset = imputer.fit_transform(X)
-    X_tr,X_te,Y_tr,Y_te=train_test_split(X,Y,test_size=0.2)
-    print (X_tr.shape,Y_tr.shape,X_te.shape,Y_te.shape)
     lr=LinearRegression()
-    model_lr=lr.fit(X_tr,Y_tr)
-    val=[1,8,1,1,2,2,2,1,1,3,3,1,1]
+    model_lr=lr.fit(X,Y)
+
+    #Data Processing
+    val=[]
+    result=req.get("result")
+    parameters=result.get("parameters")
+    for i in header:
+        str=parameters.get(i)
+        print(str)
+        val.append(str)
     ds=pd.DataFrame(val).T
+    print(ds)
     op_lrt=lr.predict(ds)
     weightage=op_lrt[0][0]
     print(weightage)
