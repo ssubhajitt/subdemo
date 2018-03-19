2464ba95d013021a9a38ab2eef786ff9718de948
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-03-19 14:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
 import pandas as pd
-from flask import Flask , request, make_response , render_template
+from flask import Flask , request, make_response , render_template, session
 from sklearn.preprocessing import Imputer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -11,29 +11,33 @@ from sklearn.decomposition import PCA
 app = Flask(__name__)
 
 @app.route('/op')
-def flask():
+def output():
     return render_template('output.html',weightage=weightage)
-
 @app.route('/')
 def homepage():
     return render_template('chatbotPage.html')
 
-@app.route('/webhook',methods=['POST'])
+@app.route('/webhook',methods=['POST','GET'])
 def webhook():
-    req=request.get_json(silent=True,force=True)
-    response=mvRegression(req)
-    res= {"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
+    if request.method == 'POST':
+        req=request.get_json(silent=True,force=True)
+        weightage=mvRegression(req)
+        session['op']=weightage
+        response="Estimated Value for the interface is : %s Do you want to try for another Interface ? (Yes/No) " %(weightage)
+        res= {"speech": response,"displayText": response,"source": "nWave-estimation-chatbot"}
+        res = json.dumps(res, indent=4)
+        print(res)
+        r = make_response(res)
+        r.headers['Content-Type'] = 'application/json'
+        return r
+    if request.method == 'GET':
+        sop=session['op']
+        return render_template('output.html',weightage=sop)
 
 def mvRegression(req):
     #Machine Learning Model
-    dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
-    #dataset=pd.read_excel("D:/Guna/POCs/ML/nWave_effort/dataset.xlsx",skip_header=1)
+    #dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset.xlsx?raw=true",skip_header=1)
+    dataset=pd.read_excel("D:/Guna/POCs/ML/nWave_effort/dataset.xlsx",skip_header=1)
     Y=dataset.iloc[:, 13:]
     X=dataset.iloc[:,1:13]
     header=list(X)
@@ -57,12 +61,11 @@ def mvRegression(req):
 
     #Prediction
     op_lrt=lr.predict(ds)
-    global weightage
-    weightage=round(op_lrt[0][0],2)
-    op="Estimated Value for the interface is : %s Person Days. Do you want to try for another Interface ? (Yes/No) " %(weightage)
+    op=round(op_lrt[0][0],2)
     print(op)
     return op
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
+        app.secret_key = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 	app.run(host='0.0.0.0', port=int(port), use_reloader=True, debug=True)
