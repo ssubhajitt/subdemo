88b66a479b0a82f1b96a612b3e86c15be0601611
ssubhajitt <41052048+ssubhajitt@users.noreply.github.com>
2018-07-16 17:12:02 +0530

    format

--- app.py
+++ app.py
@@ -6,33 +6,27 @@ import json
 import os
 import requests
 import pickle
-import pandas as pd#
+import pandas as pd
 import cloudant
-import random# from cloudant
-import Cloudant
-from docx
-import Document
-from flask
-import Flask, request, make_response, render_template, \
+import random
+from cloudant import Cloudant
+from docx import Document
+from flask import Flask, request, make_response, render_template, \
 session, g
-from sklearn.preprocessing
-import Imputer
-from sklearn.model_selection
-import train_test_split
-from sklearn.linear_model
-import LinearRegression
-from sklearn.metrics
-import mean_squared_error
-from sklearn.decomposition
-import PCA# from cloudant.error
-import CloudantException# from cloudant.result
-import Result, ResultByKey, QueryResult
+from sklearn.preprocessing import Imputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey, QueryResult
 
-# user = '9ef80d52-30b9-4e45-833a-75059db0825c-bluemix'#
+user = '9ef80d52-30b9-4e45-833a-75059db0825c-bluemix'
 password = \
-    '79d7c792bb13072da32aecc7dc3777e28780d6d11619795d54d717d2abbd62e5'#
-host = '9ef80d52-30b9-4e45-833a-75059db0825c-bluemix.cloudant.com'#
-url = 'https://' + host# client = Cloudant(user, password, url = url, connect = True)
+    '79d7c792bb13072da32aecc7dc3777e28780d6d11619795d54d717d2abbd62e5'
+host = '9ef80d52-30b9-4e45-833a-75059db0825c-bluemix.cloudant.com'
+url = 'https://' + host
+client = Cloudant(user, password, url = url, connect = True)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 
@@ -67,11 +61,13 @@ op = {
 
 print op
 
-# session = client.session()
+session = client.session()
 
 # print 'Username: {0}'.format(session['userCtx']['name'])# print 'Databases: {0}'.format(client.all_dbs())
 
-# db = client['nwaveoutput']# doc = db.create_document(op)# doc.save()
+db = client['nwaveoutput']
+doc = db.create_document(op)
+doc.save()
 c_score = confidence_score(weightage)
 
 # print doc# print c_score
