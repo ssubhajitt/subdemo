9a7d74e9a68c8a497b2e018f8b49c4d7cf13e8b5
ssubhajitt <41052048+ssubhajitt@users.noreply.github.com>
2018-07-17 20:06:15 +0530

    output

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def webhook():
     except:
         response="Critical"
     
-    res= {"speech": response,"displayText": "LOAD-PAGE","source": "nWave-estimation-chatbot"}
+    res= {"output": response}
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
