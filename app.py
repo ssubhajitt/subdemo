d76e39e2490c26aadcd52675fe9357a4d996459f
ssubhajitt <41052048+ssubhajitt@users.noreply.github.com>
2018-07-18 15:40:26 +0530

    or2

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def webhook():
 
         # response = 'OK'
 
-        if (weightage < 0.35 and c_score > 80):
+        if (weightage < 0.35 or c_score > 80):
             response = 'Critical'
         else:
             response = 'OK'
